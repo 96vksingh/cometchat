a2699a934c48bd2e1b7bd2dcf300ae9e88a4f051
Vishal Singh <vk@96vksingh.local>
2021-04-20 22:30:02 +0530

    first commit

---
+++ app/src/main/java/com/cometchat/pro/androiduikit/UIKitApplication.java
@@ -0,0 +1,93 @@
+package com.cometchat.pro.androiduikit;
+
+import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+import android.util.Log;
+import android.widget.Toast;
+
+
+import com.cometchat.pro.core.AppSettings;
+import com.cometchat.pro.core.CometChat;
+import com.cometchat.pro.exceptions.CometChatException;
+import com.cometchat.pro.androiduikit.constants.AppConfig;
+import com.cometchat.pro.uikit.ui_components.calls.call_manager.listener.CometChatCallListener;
+import com.cometchat.pro.uikit.ui_settings.UIKitSettings;
+
+
+public class UIKitApplication extends Application {
+
+    private static final String TAG = "UIKitApplication";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        AppSettings appSettings = new AppSettings.AppSettingsBuilder().
+                subscribePresenceForAllUsers().setRegion(AppConfig.AppDetails.REGION).build();
+        CometChat.init(this, AppConfig.AppDetails.APP_ID, appSettings,
+                new CometChat.CallbackListener<String>() {
+            @Override
+            public void onSuccess(String s) {
+                UIKitSettings.setAppID(AppConfig.AppDetails.APP_ID);
+                UIKitSettings.setAuthKey(AppConfig.AppDetails.AUTH_KEY);
+                CometChat.setSource("ui-kit","android","java");
+                Log.d(TAG, "onSuccess: "+s);
+            }
+
+            @Override
+            public void onError(CometChatException e) {
+                Toast.makeText(UIKitApplication.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        CometChatCallListener.addCallListener(TAG,this);
+        createNotificationChannel();
+    }
+
+    private void addConnectionListener(String tag) {
+        CometChat.addConnectionListener(tag, new CometChat.ConnectionListener() {
+            @Override
+            public void onConnected() {
+                Toast.makeText(getBaseContext(),"Connected",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onConnecting() {
+
+            }
+
+            @Override
+            public void onDisconnected() {
+                Toast.makeText(getBaseContext(),"You have been disconnected.",Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onFeatureThrottled() {
+
+            }
+        });
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.app_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel channel = new NotificationChannel("2", name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+        CometChatCallListener.removeCallListener(TAG);
+    }
+}
---
+++ app/src/main/java/com/cometchat/pro/androiduikit/constants/AppConfig.java
@@ -0,0 +1,14 @@
+package com.cometchat.pro.androiduikit.constants;
+
+public class AppConfig {
+
+    public class AppDetails {
+
+        public static final String APP_ID = "315329889cc5bd0";
+
+        public static final String AUTH_KEY = "e54c53f922dfb19cdf9fd6d60ee49fd24366fd5c";
+
+        public static final String REGION = "us";
+    }
+}
+
